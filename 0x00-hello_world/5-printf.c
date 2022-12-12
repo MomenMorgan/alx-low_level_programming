9424cb0e05f7228c0733dcb847cb96719db2f93a
MomenMorgan <mominmorgan10@gmail.com>
2022-12-12 10:47:21 -0800

    f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+/**
+*Description: main - print string
+*Return 0 if sucess
+*/
+{
+printif("\"with proper grammar, but the outcome is a piece of art,")
+return(0)
+}
