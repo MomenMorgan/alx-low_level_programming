ae653b3d0b154fccb4f4168d173d5df26b4e6193
MomenMorgan <mominmorgan10@gmail.com>
2022-12-12 10:50:23 -0800

    f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@ int main(void)
 *Return 0 if sucess
 */
 {
-printif("\"with proper grammar, but the outcome is a piece of art,")
+printif("with proper grammar, but the outcome is a piece of art,\n")
 return(0)
 }
