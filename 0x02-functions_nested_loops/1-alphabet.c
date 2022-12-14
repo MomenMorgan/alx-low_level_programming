b6d1584dbbc53de49f216175c43069d108e76dc5
MomenMorgan <mominmorgan10@gmail.com>
2022-12-14 09:13:26 -0800

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+
+* main - check the code*
+
+* Return: Always 0.
+
+*/
+
+void print_alphabet(void)  
+{
+  
+int n;
+
+
+for (n = 'a'; n <= 'z'; n++)    
+{
+
+putchar(n);
+      
+}
+  
+putchar('\n');
+  
+}
