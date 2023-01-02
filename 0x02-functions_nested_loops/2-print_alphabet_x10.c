be6b054dc422b6155a860da804668de0841b442c
MomenMorgan <mominmorgan10@gmail.com>
2023-01-02 08:36:40 -0800

    f

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ *print_alphabetx10
+ *return x10 a-z
+ */
+
+void print_alphabet_x10(void)
+{
+	int n,g;
+	g = 0;
+	while (g < 10){
+		for(n='a' ; n<= 'z'; n++)
+		{
+		       _putchar(n);
+
+		}
+               _putchar('\n');
+	}		g++;
+}
