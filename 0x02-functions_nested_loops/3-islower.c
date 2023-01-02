2665a67cc450695a5390f261346dcfd68e6eb30e
MomenMorgan <mominmorgan10@gmail.com>
2023-01-02 09:40:29 -0800

    f

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "mian.h"
+
+/**
+*_islowe - checks if the letter lowercase or not
+*function return 1 or 0 for true or false
+*/
+
+int _islower(int c)
+{
+
+ if (c >= 'a' && c <= 'z')
+
+return (1);
+else
+return (0);
+}
