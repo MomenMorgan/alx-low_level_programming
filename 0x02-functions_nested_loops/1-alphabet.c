9895c6e79e7b36fecf898a20c9dcf35b8d1ff3b4
MomenMorgan <mominmorgan10@gmail.com>
2023-01-02 08:08:14 -0800

    f

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,21 @@
 * print_alphabet - print all alphabet in lowercase
 */
 void print_alphabet(void)
+
 {
 
-int letter;
-for (letter = 'a';letter <= 'z';letter++)
-{
-_putchar(letter);
-}
-_putchar("\n");
+	int n;
+
+
+
+	for (n = 'a'; n <= 'z'; n++)
+
+	{
+
+		_putchar(n);
+
+	}
+
+	_putchar('\n');
+
 }
