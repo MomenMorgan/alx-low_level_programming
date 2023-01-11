55b62674ed63fa9c01a2d4e181dd394d4a0ce70e
MomenMorgan <mominmorgan10@gmail.com>
2023-01-11 12:32:18 -0800

    f

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -8,45 +8,37 @@
 */
 
 unsigned int _strspn(char *s, char *accept)
+{
+int i, j, f, flag;
+f = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+
+flag = 0;
+for (j = 0; accept[j] != '\0'; j++)
 
 {
 
-	int i, j, f, flag;
+if (s[i] == accept[j])
 
+{
 
+f++;
 
-	f = 0;
+flag = 1;
 
+}
 
-	for (i = 0; s[i] != '\0'; i++)
+}
 
-	{
+if (flag == 0)
 
-		flag = 0;
-		for (j = 0; accept[j] != '\0'; j++)
+{
 
-		{
+return (f);
+}
 
-			if (s[i] == accept[j])
-
-			{
-
-				f++;
-
-				flag = 1;
-
-			}
-
-		}
-
-		if (flag == 0)
-
-		{
-
-			return (f);
-		}
-
-	}
+}
 
 
 
