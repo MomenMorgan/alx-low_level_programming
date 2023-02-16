eed4509b01f284b225b7f1ddface559e2daa909c
momin <mominmorgan0@gmail.com>
2023-02-16 13:09:37 -0800

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
 *main - strn
 *Return: Always 1 (success)
@@ -8,7 +8,7 @@
 int main(void)
 {
 
-write(2, "and that piece of art is useful- Dora Korpar, 2015-10-19\n", 62);
+write(2, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 62);
 
 return (1);
 }
