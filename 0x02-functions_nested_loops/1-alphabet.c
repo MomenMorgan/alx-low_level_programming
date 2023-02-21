fc812a8389a0f64fec315136203d7b55a419a865
momin <mominmorgan0@gmail.com>
2023-02-21 10:00:12 -0800

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#includ "main.h"
+#include "main.h"
 
 /**
 * print_alphabet - prints the alphabet in lowercase
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
 
 char letter;
-for(letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
 
