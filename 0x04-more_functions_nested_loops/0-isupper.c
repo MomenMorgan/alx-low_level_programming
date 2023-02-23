9d13c7a37cd40eafcdf6babce4ba151734a1bd12
momin <mominmorgan0@gmail.com>
2023-02-23 07:08:07 -0800

    gg

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *int _isupper - for uppercase letter
+ * Return: 1 for upper 0 otherwise
+ * @c: varible
+ */
+
+
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+
+{
+return (1);
+}
+else
+return (0);
+}
