1273df4053b88b21ea3024f84e4029e76c361a52
momin <mominmorgan0@gmail.com>
2023-02-25 04:52:50 -0800

    gg

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ *@size: varible.
+ *Return: 0 always
+ */
+
+void print_triangle(int size)
+{
+int r , s ,d;
+
+for (r = 1 ; r <= size ; r++)
+{
+for (s = r ; s <= size ; s++)
+_putchar(' ');
+
+for (d = 0; d <= r ; d)
+_putchar('#');
+if ( size <= 0)
+_putchar('\n');
+
+_putchar('\n');
+}
+}
