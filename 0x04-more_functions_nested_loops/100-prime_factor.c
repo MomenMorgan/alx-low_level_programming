867bc3283eeee0a0f96ac0ed5db2d8ed6f98e323
momin <mominmorgan0@gmail.com>
2023-02-26 12:45:15 -0800

    gg

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print the largest prime number
+ *Return: 0 success
+ */
+
+int main(void)
+{
+
+long num = 612852475143;
+int p;
+
+for (p = 2; p > 1 ; p++)
+{
+while ((num % p) == 0)
+{
+if ((num / p) == 1)
+printf("%d\n", p);
+num /= p;
+}
+}
