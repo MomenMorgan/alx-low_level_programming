461f09e566a9edc3aa39634b28d1bfe37bac3dea
momin <mominmorgan0@gmail.com>
2023-02-26 12:59:51 -0800

    gg

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,14 +9,14 @@ int main(void)
 {
 
 long num = 612852475143;
-int p;
+long p;
 
 for (p = 2; p > 1 ; p++)
 {
 while ((num % p) == 0)
 {
 if ((num / p) == 1)
-printf("%d\n", p);
+printf("%ld\n", p);
 num /= p;
 }
 }
