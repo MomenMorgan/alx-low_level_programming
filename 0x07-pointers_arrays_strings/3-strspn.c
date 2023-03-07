365117f4cb756c3c402ea299a3eba5b80e47d6bd
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-07 14:16:53 +0200

    hh

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,24 +9,26 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int check;
-unsigned int i = 0;
-unsigned int j = 0;
-unsigned int coun = 0;
+int check;
+int i = 0;
+int j = 0;
+int coun = 0;
 while (s[i] != '\0')
 {
 i++;
 while (accept[j] != '\0')
 {
 j++;
-if (s[i] == accept[j])
+if (s[i] ==  accept[j])
 {
 coun++;
-check =1;
+check = 1;
 }
 }
-if(coun == 0)
+if (coun == 0)
+{
 return (coun);
+}
 j = 0;
 }
 return (coun);
