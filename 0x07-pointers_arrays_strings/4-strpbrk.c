2a7c9ceab4c3268f913ebcb49c6d626ae81542d5
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-07 16:09:17 +0200

    hh

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s:input
+ * @accept:input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+char *q = accept;
+while (*s != '\0')
+{
+while (*q != '\0')
+{
+if (*s == *q)
+{
+return (s);
+}
+
+*q++;
+}
+q = accept;
+*s++;
+}
+return (NULL);
+}
