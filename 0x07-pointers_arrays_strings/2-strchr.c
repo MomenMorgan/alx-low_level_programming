3049f66668dab83e8c07d2deab1e163691670ad9
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-07 10:34:37 +0200

    gg

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strchr - search for a char.
+ *@s: varible.
+ *@c: varible.
+ * Return: pointer to s or null of none.
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+*s++;
+}
+return (NULL);
+}
