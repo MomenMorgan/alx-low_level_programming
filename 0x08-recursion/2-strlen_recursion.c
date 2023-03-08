459f39c9c9fee91daf6de68f512ab46dbe0ab829
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-08 19:31:15 +0200

    gg

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print the length of string.
+ * @s: prm;
+ * Return: Always 0 (Success)
+ */
+
+int _strlen_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+return (0);
+
+}
+else
+return (1 + _strlen_recursion(s + 1));
+}
