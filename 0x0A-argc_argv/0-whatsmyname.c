b23d8a9af0ada6f0c5bd4f271649e895147938c5
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-11 19:08:55 +0200

    file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the compiled file name.
+ *@argc:input
+ *@argv:input
+ *Return: 0 success.
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+
+return (0);
+}
