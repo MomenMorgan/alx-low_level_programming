bf3ae39917c1a6f7f7a3d64d04ad6d11df3a14ab
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-13 19:19:56 +0200

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,10 +9,10 @@
 
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
 char *x = (char *)malloc(sizeof(c) * size);
 if (size == 0)
 return (NULL);
-unsigned int i;
 for (i = 0; i < size; i++)
 {
 x[i] = c;
