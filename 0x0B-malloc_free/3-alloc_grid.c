cec352dd00141459fac6f80e256cc2e6b20ca0fd
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-14 20:07:52 +0200

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+
+/**
+ *alloc_grid - dynamic 2d array alloc.
+ *@width: arrays size
+ *@height:arrays count.
+ * Return: pointer to pointer of arrays or NULL on Error
+ */
+
+int **alloc_grid(int width, int height)
+{
+int i;
+int j = 0;
+int **arr;
+if (width <= 0 || height <= 0)
+return (NULL);
+arr = malloc(sizeof(int *) * height);
+if (arr == 0)
+return (0);
+for (i = 0 ; i < height ; i++)
+{
+arr[i] = malloc(sizeof(int) * width);
+}
+for (i = 0 ; i < height; i++)
+{
+for (j = 0 ; j < width; j++)
+{
+arr[i][j] = 0;
+}
+}
+return (arr);
+}
