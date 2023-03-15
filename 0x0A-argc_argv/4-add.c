5222311c1bb2b26394c386319231e1dd6f6b50c2
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-15 13:27:17 +0200

    mul

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,14 +9,27 @@
  */
 int main(int argc, char *argv[])
 {
-int mul;
-
-if (argc > 3 || argc < 3)
+int sum = 0;
+int i = 0;
+if (argc == 1)
 {
-printf("Error\n");
-return (1);
+printf("0\n");
+return (0);
 }
-mul = atoi(argv[1]) * atoi(argv[2]);
-printf("%d\n", mul);
+
+while(argc--)
+{
+i++;
+if(argc == 0)
+break;
+else if (argv[i] > '9' )
+{
+puts("Error");
+return (1);
+}    
+*argv++;
+sum += atoi(*argv);
+}
+printf("%d\n", sum);
 return (0);
 }
