a8a27595a9f844544153e04fd6193a3a5bafd4d4
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-15 16:29:10 +0200

    Lol

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 int i;
+int j;
 int sum = 0;
 if (argc < 2)
 {
@@ -18,11 +19,17 @@ return (0);
 }
 for (i = 1 ; i < argc; i++)
 {
-if (atoi(argv[i]) > 9 || atoi(argv[i]) < 1)
+for (j = 0; j != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("Error\n");
 return (1);
 }
+}
+}
+for (i = 1; i < argc; i++)
+{
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
