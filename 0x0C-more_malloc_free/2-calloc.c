acf658c676e6054dfe665fc3fc0e9b01bf91fd96
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-16 20:14:21 +0200

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an araay.
+ * @nmemb: amount.
+ * @size: type.
+ * Return: pointer to space in meemory or null for error.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	int *x;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+
+		return (NULL);
+
+	x = malloc(sizeof(size) * nmemb);
+	if (x == 0)
+		return (NULL);
+	i = 0;
+	while (i < nmemb)
+	{
+		x[i] = 0;
+		i++;
+	}
+
+	return (x);
+}
