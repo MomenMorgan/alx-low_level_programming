78cd137067fa7e9b1e811daebca8aacab854d0d5
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-16 21:47:37 +0200

    ggg

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,11 +15,14 @@ int *array_range(int min, int max)
 {
 	int *x;
 	int i;
+	int len;
 
 	if (min > max)
 	return (NULL);
 
-	x = malloc(sizeof(min) * (min - max) + 1);
+	len = max - min + 1;
+
+	x = malloc(sizeof(min) * len + 1);
 	if (x == NULL)
 		return (NULL);
 
