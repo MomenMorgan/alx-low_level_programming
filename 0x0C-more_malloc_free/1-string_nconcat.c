18a2274ec4ae306264ddf08a6d1150c4e98021e7
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-16 16:08:10 +0200

    concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+/**
+ *string_nconcat - concatenate s1 to n of s2;
+ *@s1: first string
+ *@s2: second string
+ *@n:length to be copied
+ *Return: pointer to x or NULL for Error.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *
+int len, i, j;
+len = strlen(s1);
+
+x = malloc(sizeof(char) * (len + n) + 1);
+if (x == 0)
+{
+	return (NULL);
+}
+
+i = 0;
+j = 0;
+while (*s1 != '\0')
+{
+	x[i] = *s1;
+	s1++;
+	i++;
+}
+while (n != 0)
+{
+	x[i] = *s2;
+	i++;
+	s2++;
+	n--;
+}
+
+x[i] = '\0';
+return (x);
+}
