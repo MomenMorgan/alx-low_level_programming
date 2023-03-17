4e90c115291c0f06316feb2b7c9943bac1fc3bd7
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-17 12:12:57 +0200

    buffer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - print n of buffer
+ * @s: array .
+ * @b:char.
+ * @n: number to be edited.
+ * Return: pointer to the calling function.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
