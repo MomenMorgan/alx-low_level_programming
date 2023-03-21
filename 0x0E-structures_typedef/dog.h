68d924f654399da141d2ed95cb6e12f32707ade5
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-21 21:34:24 +0200

    init for dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,3 +13,11 @@ float age;
 char *owner;
 };
 #endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
