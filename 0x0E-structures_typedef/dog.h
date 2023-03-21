e469e3971177fb01b5cf3f34d8e0a790ec3f3c2f
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-21 21:43:08 +0200

    dog init struct fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRUCTS
+#define STRUCTS
 /**
  * struct dog - structure for dog info.
  * @name: varible
  * @age: varible
  * @owner:varible
  */
-struct  dog
+typedef struct  dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 #endif
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
@@ -20,4 +20,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
