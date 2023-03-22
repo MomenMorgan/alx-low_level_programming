bbe3bcd3db7e6afffb1cc987d557d999996fce7e
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-22 16:08:42 +0200

    function pointer to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+
+/**
+ * print_name - prints a name as is
+ * @f:FUNCTION POINTER.
+ * @name: name of the person
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	 (*f)(name);
+}
