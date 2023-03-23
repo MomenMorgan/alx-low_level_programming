9045deead202809d41b9d937e9c82823b24bb699
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-23 16:13:08 +0200

    prit numbers with a separator fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,25 +11,29 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list x;
-	unsigned int i, l;
+	unsigned int i , l;
 
 	va_start(x, n);
 
-	if (separator == NULL)
-		separator = " ";
 
 	for (i = 0; i < n; i++)
 	{
-		l =  va_arg(x, int);
-		printf("%d", l);
-		if (i == n - 1)
-			continue;
+		l =  va_arg(x , int);
 
-		printf("%c", *separator);
-		printf(" ");
+		if(separator == NULL)
+			printf("%d" , l);
+
+		else if(separator)
+		{
+			printf("%d",l);
+
+			if (i == n-1)
+				continue;
+
+			printf("%c", *separator);
+			printf(" ");
+		}
+
 	}
-
 	printf("\n");
-	va_end(x);
-
 }
