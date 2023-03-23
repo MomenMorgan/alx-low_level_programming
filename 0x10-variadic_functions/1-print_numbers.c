f44f020e55d7a26b472cd1d7aa5c4682d996ce5d
Momen MOrgan <mominmorgan10@gmail.com>
2023-03-23 15:58:38 +0200

    prit numbers with a separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers with a separator.
+ * @separator: separator between numbers.
+ * @n: parameter numbers.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list x;
+	unsigned int i, l;
+
+	va_start(x, n);
+
+	if (separator == NULL)
+		separator = " ";
+	for (i = 0; i < n; i++)
+	{
+		l =  va_arg(x, int);
+		printf("%d", l);
+		if (i == n - 1)
+			continue;
+
+		printf("%c", *separator);
+		printf(" ");
+	}
+
+	printf("\n");
+
+}
