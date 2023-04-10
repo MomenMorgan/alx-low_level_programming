df1feb2dc69a79ffa1ccddc1407b36cde14da350
Momen MOrgan <mominmorgan10@gmail.com>
2023-04-10 22:05:14 +0200

    get bit value at certain index fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,7 +15,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int cp;
 
 
-	if ( index > 64)
+	if (index > 64)
 	{
 
 		return (-1);
@@ -37,5 +37,5 @@ int get_bit(unsigned long int n, unsigned int index)
 			return (0);
 	}
 
-	return (-1);
+	return (n & 1);
 }
