8a6635377446237455c51a5fb44e5dc6d1fcec17
Momen MOrgan <mominmorgan10@gmail.com>
2023-04-10 16:47:42 +0200

    hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint - converts binary to base 10 number
  * @b: string of binary char.
- * return - base 10 number or 0 if there is one or more char.
+ * Return: base 10 number or 0 if there is one or more char.
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -12,25 +12,27 @@ unsigned int binary_to_uint(const char *b)
 	int base = 1;
 	int res = 0;
 
-	for(i = 0; b[i] != '\0'; i++);
 
-	while((i-1) >= 0)
+	for (i = 0; b[i] != '\0'; i++)
+		;
+
+	while ((i - 1) >= 0)
 	{
 		i--;
 
-		if(b[i] == '1')
+		if (b[i] == '1')
 		{
 			res += base;
 
 		}
 		/*if there is one char or more return 0;*/
-		else if(b[i] > '1')
+		else if (b[i] > '1')
 		{
-			return 0;
+			return (0);
 		}
 
 		base = base * 2;
 
 	}
-	return res;
+	return (res);
 }
