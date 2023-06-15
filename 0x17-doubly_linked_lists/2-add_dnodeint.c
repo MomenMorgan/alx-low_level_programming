3c8a86c1485732fa5c5bbf5fe4efdd2091095f72
momin morgan <mominmorgan10@gmail.com>
2023-06-15 12:47:06 -0700

    new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,19 +1,24 @@
 #include "lists.h"
 
 /**
- * dlistint_len - return the number of elements in linked list
- * @h: The head of the list
- * Return: returns the number of nds
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: pointer to the head of the list
+ * @n: new int node
+ * Return: null for faliur new nodes adress for success
  */
-size_t dlistint_len(const dlistint_t *h)
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	size_t nds = 0;
+	dlistint_t *new;
 
-	while (h)
-	{
-		nds++;
-		h = h->next;
-	}
-
-	return (nds);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
 }
