1308dad93ce87f456ab9c0d94a067ec628c69dea
momin morgan <mominmorgan10@gmail.com>
2023-06-15 12:56:23 -0700

    fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *start, *last;
 
-	new = malloc(sizeof(dlistint_t));
+	start = malloc(sizeof(dlistint_t));
 	if (start == NULL)
 		return (NULL);
 
