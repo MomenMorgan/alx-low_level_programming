9c972fbf46283cd467b7df75595c97a7c9467035
momin morgan <mominmorgan10@gmail.com>
2023-06-15 13:07:17 -0700

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint -  the sum of all the data (n) of a dlistint_t linked list.
+ * @head: head node o list
+ * Return: The sum of data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -22,7 +22,7 @@ typedef struct dlistint_s
 } dlistint_t;
 
 
-
+int sum_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
