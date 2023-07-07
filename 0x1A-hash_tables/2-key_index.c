b786e3e217183b8e1c619d085de399697f9f2357
momin morgan <mominmorgan10@gmail.com>
2023-07-07 15:16:28 -0700

    returns index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - findes the index of key-value
+ * @key: key (string)
+ * @size: size of array of hash table
+ * Return: index at where the key-value stored 
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
