906b0c4494ea281fc24b2f628d8f826cea85e095
momin morgan <mominmorgan10@gmail.com>
2023-10-11 14:52:19 -0700

    fixing print function

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,27 @@
 #include "search_algos.h"
 
 /**
+ * print_array - prints an array of integers
+ * @array: pointer to the first element of the array to print
+ * @low: first index to print
+ * @high: last index to print
+ */
+
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+	{
+		printf("%d", array[i]);
+		if (i < high)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
  * exponential_search - searches for a value in a sorted array of integers
  * using the Exponential search algorithm
  * @array: pointer to the first element of the array to search in
@@ -43,3 +64,5 @@ int exponential_search(int *array, size_t size, int value)
 
 	return (-1);
 }
+
+
