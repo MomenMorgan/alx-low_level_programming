e8bf4eb22a85b4dd65296af6520965d62da7eab4
momin morgan <mominmorgan10@gmail.com>
2023-10-11 13:38:14 -0700

    fix heaDER

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,9 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
