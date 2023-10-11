19d1dbf65086db4c385e4679dff20f9aff11c8b2
momin morgan <mominmorgan10@gmail.com>
2023-10-11 13:22:43 -0700

    hot fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,21 +12,20 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        if (array == NULL)
-                return (-1);
+	if (array == NULL)
+		return (-1);
 
-        while (i < size)
-        {
-                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-                if (array[i] == value)
-                        return (i);
+		if (array[i] == value)
+			return (i);
 
-                i++;
-        }
+		i++;
+	}
 
-        return (-1);
+	return (-1);
 }
-
